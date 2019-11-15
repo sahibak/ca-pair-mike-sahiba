f407aed3a73419980097b8eb60024ad2174ce362
Sahiba K <khanna.sahiba@gmail.com>
2019-11-15 11:15:56 -0500

    base functional file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Card from "./component.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Card></Card>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./style.scss";
+
+export default class Game extends React.Component {
+   state = {
+       values: [,,,,,,,,],
+       counter: 0,
+       nextStep: true
+   } 
+  
+
+   submitFunc(event,n){ 
+       event.preventDefault();    
+        let list = this.state.values;
+        let display = this.state.nextStep?"X":"O";
+        console.log(display);
+        list[n] = display;
+        let newlist= list
+        console.log("original",list);
+        console.log("newList",newlist)
+           this.setState({
+               counter: this.state.counter + 1,
+               nextStep: !this.state.nextStep,
+               values: newlist
+           })
+           console.log(this.state)
+       }
+
+       displayFunc(i){
+           let arrayOfResponses = this.state.values;
+           if (!arrayOfResponses[i]){
+                return "default-class"
+           } else if (arrayOfResponses[i] = "X") {
+                return "X-class"
+            } else {
+                return "class-O"
+            }}
+
+
+
+    render(){
+        return(
+            <>
+            <button onClick={(e)=> this.submitFunc(e, 0)} className = {this.displayFunc(0)}
+            >test1</button>
+            <button onClick={(e)=> this.submitFunc(e, 1)} className = {this.displayFunc(1)}
+            >test2</button>
+            <button name = "Q1" onClick={(e)=> this.submitFunc(e, 2)} className = {this.displayFunc(2)}
+            >test3</button>
+            <button name = "Q1" onClick={(e)=> this.submitFunc(e, 3)} className = {this.displayFunc(4)}>test3</button>
+          </>
+        )
+
+    }
+}

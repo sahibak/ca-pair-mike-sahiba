3e74408ffb1f92b051b0826a0ba9089a6fe8f87d
Mike Beaudry <michaelbeaudry93@gmail.com>
2019-11-15 14:41:18 -0500

    another version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import Card from "./component.js";
+import Homepage from "./assets/homepage"
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Card></Card>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={Homepage}></Route>
+            <Route path="/best-game-ever" component={Card}></Route>
+          </Switch>
+        </BrowserRouter>
       </header>
     </div>
   );
---
+++ src/assets/homepage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class homepage extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Tic Tac Toe </h1>
+                <button></button>
+            </div>
+        )
+    }
+}
+
+export default homepage
